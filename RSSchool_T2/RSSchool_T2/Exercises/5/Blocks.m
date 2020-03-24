d9f4e9e153b190cab5b04243cc93aae9862d1a7f
Dmitriy S <sutyginds@gmail.com>
2020-03-24 16:27:08 +0300

    fix: build

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,17 +2,7 @@
 
 @implementation Blocks
 
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        self.blockA(<#NSArray *#>) = ^(NSArray *array){
-            if (array.count == 0) {
-            }
-        }
-    }
-    return self;
-}
+
 
 @end
 
