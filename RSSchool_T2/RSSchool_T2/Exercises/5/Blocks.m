98feb8149e1778a3d7755f2b40773425b35d419e
Dmitriy S <sutyginds@gmail.com>
2020-03-30 00:56:38 +0400

    feat task5

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,7 +2,72 @@
 
 @implementation Blocks
 
+NSArray *blocksArray;
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+- (BlockA)blockA {
+    
+    return ^(NSArray *array){
+        if (array.count != 0) {
+            blocksArray = array;
+        }
+    };
+}
+
+- (BlockB)blockB {
+    
+    __weak Blocks *weakSelf = self;
+    
+    BlockB blockB = ^(Class class){
+        
+        if (class == [NSString class]) {
+            
+            NSMutableString *result = [[NSMutableString alloc] init];
+            for (int i=0; i<blocksArray.count; i++) {
+                
+                if ([blocksArray[i] isKindOfClass:class]) {
+                    [result appendString:blocksArray[i]];
+                }
+            }
+            weakSelf.blockC(result);
+        };
+        
+        if (class == [NSNumber class]) {
+            int sum = 0;
+            for (int i=0; i<blocksArray.count; i++) {
+                if ([blocksArray[i] isKindOfClass:class]) {
+                    sum = sum + [blocksArray[i] intValue];
+                }
+            }
+            weakSelf.blockC([NSNumber numberWithInt:sum]);
+        }
+                
+        if (class == [NSDate class]) {
+            
+            NSDate *date = [[NSDate alloc]init];
+            NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+            formatter.dateFormat = @"dd.MM.yyyy";
+            for (int i=0; i<blocksArray.count; i++) {
+                if ([blocksArray[i] isKindOfClass:class]) {
+                    if (blocksArray[i] > date) {
+                        date = blocksArray[i];
+                    }
+                }
+            }
+            weakSelf.blockC([formatter stringFromDate:date]);
+        }
+                
+    };
+    
+    return [blockB copy];
+}
 
 @end
 
